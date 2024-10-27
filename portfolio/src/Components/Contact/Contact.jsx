a35d5970eb55991950eb6e501483b8d9f33b2e72
sarathkumar s <sarathkumar93622@gmail.com>
2024-10-27 11:06:13 +0530

    form submission completed working on media quary to make the webpage responsive

--- portfolio/src/Components/Contact/Contact.jsx
+++ portfolio/src/Components/Contact/Contact.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './Contact.css'
 import theme_pattern from '../../assets/theme_pattern.Svg'
 import mail_icon from '../../assets/mail_icon.Svg'
@@ -6,6 +6,30 @@ import location_icon from '../../assets/location_icon.Svg'
 import call_icon from '../../assets/call_icon.Svg'
 
 const Contact = () => {
+
+  const [result,setResult ] = useState("");
+
+  const onSubmit = async (event) => {
+    event.preventDefault();
+    setResult("Sending....");
+    const formData = new FormData(event.target);
+
+    formData.append("access_key", "151efa5e-2747-4bd2-a3c6-240d8f73452b");
+
+    const response = await fetch("https://api.web3forms.com/submit", {
+      method: "POST",
+      body: formData
+    });
+
+    const data = await response.json();
+
+    if (data.success) {
+      alert(data.message)
+    } else {
+      console.log("Error", data);
+      setResult(data.message);
+    }
+  };
   return (
     <div id='contact' className='contact'>
       <div className="contact-title">
@@ -31,11 +55,11 @@ const Contact = () => {
                 </div>
              </div>
         </div>
-        <form className='contact-right'>
+        <form onSubmit={onSubmit} className='contact-right'>
           <label htmlFor="">Your Name</label>
-          <input type="text" placeholder='Enter Your Name' name='name' />
+          <input type="text" placeholder='Enter Your Name' name='name' required/>
            <label htmlFor="">Your Email</label>
-           <input type="email" placeholder='Enter Your Email'/>
+           <input type="email" placeholder='Enter Your Email' name='email' required/>
            <label htmlFor="Write Your Message Here"></label>
            <textarea name="message" rows="8" placeholder='Enter Your Message'></textarea>
            <button type='submit' className='contact-submit'>Submit now</button>
